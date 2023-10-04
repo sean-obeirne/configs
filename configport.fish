37ce779524d4949ebf17744cb44ad554a5bea3f8
sean-obeirne <sean.t.obeirne@gmail.com>
2023-10-04 17:26:33 -0400

    added support for portastation

---
+++ configport.fish
@@ -0,0 +1,25 @@
+set fish_greeting ""
+
+alias cdp='cd ~/code/playbook/nycbbl'
+alias cdpa='cd ~/code/playbook; bash'
+alias cdm='cd ~/code/tfg/Mission-Uplink'
+alias code='snap run code'
+alias vno='nvim ~/code/playbook/notes'
+alias vnv='nvim ~/.config/nvim/init.lua'
+alias nvp='nvim ~/code/playbook/nycbbl'
+alias nvm='nvim ~/code/tfg/Mission-Uplink'
+alias n='nvim .'
+
+# git shortcuts
+alias pul='git pull'
+alias pus='git push'
+alias com='git commit -m'
+alias gad='git add'
+alias gadall='git add *'
+alias gst='git status'
+alias gcm='git checkout master'
+
+# config editors
+alias frc='nvim ~/.config/fish/config.fish'
+alias nrc='nvim ~/.config/nvim/init.lua'
+alias arc='nvim ~/.config/alacritty/alacritty.yml'
